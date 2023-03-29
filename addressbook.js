3c6f2920ae77acef1bfcf32e1136230bfc87bf33
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-29 13:44:04 +0530

    Add : UC7 - Address Book

--- addressbook.js
+++ addressbook.js
@@ -1,27 +1,19 @@
-let contacts = [
-    { name: "John Smith", email: "john@example.com", phone: "123-456-7890" },
-    { name: "Jane Doe", email: "jane@example.com", phone: "987-654-3210" },
-    { name: "Bob Johnson", email: "bob@example.com", phone: "555-555-5555" }
-  ];
-  
-  // Function to delete a contact by name
-  function deleteContact(name) {
-    let index = -1;
-    for (let i = 0; i < contacts.length; i++) {
-      if (contacts[i].name === name) {
-        index = i;
-        break;
-      }
-    }
-    if (index !== -1) {
-      contacts.splice(index, 1);
-      console.log(`Deleted contact: ${name}`);
-    } else {
-      console.log(`Contact not found: ${name}`);
-    }
+let addressBook = [];
+
+function addPerson(name, email, phone) {
+  // check if person already exists in address book
+  const isDuplicate = addressBook.filter(person => person.name === name).length > 0;
+
+  // add person to address book if not a duplicate
+  if (!isDuplicate) {
+    addressBook.push({ name, email, phone });
+    console.log(`Added ${name} to address book.`);
+  } else {
+    console.log(`${name} already exists in address book.`);
   }
-  
-  // Example usage
-  deleteContact("Jane Doe");
-  console.log(contacts);
-  
+}
+
+// example usage
+addPerson('John Smith', 'john@example.com', '555-1234');
+addPerson('Jane Doe', 'jane@example.com', '555-5678');
+addPerson('John Smith', 'john.smith@example.com', '555-4321'); // this will be a duplicate entry

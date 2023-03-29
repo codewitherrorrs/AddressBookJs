8abdba218398be952fba083817b09db7013e59cc
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-29 13:52:11 +0530

    Add : UC12 - Address Book

--- addressbook.js
+++ addressbook.js
@@ -1,32 +1,66 @@
-const contacts = [
-    { name: 'John Doe', city: 'New York', state: 'New York' },
-    { name: 'Jane Smith', city: 'Los Angeles', state: 'California' },
-    { name: 'Bob Johnson', city: 'Chicago', state: 'Illinois' },
-    { name: 'Mike Williams', city: 'Chicago', state: 'Illinois' },
-    { name: 'Mary Davis', city: 'San Francisco', state: 'California' },
-    { name: 'David Lee', city: 'New York', state: 'New York' }
+// Sample array of address book entries
+const addressBook = [
+    {
+      name: 'John Smith',
+      city: 'San Francisco',
+      state: 'CA',
+      zip: '94107'
+    },
+    {
+      name: 'Jane Doe',
+      city: 'Los Angeles',
+      state: 'CA',
+      zip: '90012'
+    },
+    {
+      name: 'Bob Johnson',
+      city: 'New York',
+      state: 'NY',
+      zip: '10001'
+    }
   ];
   
-  // Get count of contacts by city
-  const contactsByCity = contacts.reduce((acc, curr) => {
-    if (acc[curr.city]) {
-      acc[curr.city]++;
+  // Sort function by city
+  function sortByCity(a, b) {
+    if (a.city < b.city) {
+      return -1;
+    } else if (a.city > b.city) {
+      return 1;
     } else {
-      acc[curr.city] = 1;
+      return 0;
     }
-    return acc;
-  }, {});
+  }
   
-  console.log('Contacts by city:', contactsByCity);
-  
-  // Get count of contacts by state
-  const contactsByState = contacts.reduce((acc, curr) => {
-    if (acc[curr.state]) {
-      acc[curr.state]++;
+  // Sort function by state
+  function sortByState(a, b) {
+    if (a.state < b.state) {
+      return -1;
+    } else if (a.state > b.state) {
+      return 1;
     } else {
-      acc[curr.state] = 1;
+      return 0;
     }
-    return acc;
-  }, {});
+  }
   
-  console.log('Contacts by state:', contactsByState);
+  // Sort function by zip
+  function sortByZip(a, b) {
+    if (a.zip < b.zip) {
+      return -1;
+    } else if (a.zip > b.zip) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  
+  // Sort the address book by city
+  addressBook.sort(sortByCity);
+  console.log(addressBook);
+  
+  // Sort the address book by state
+  addressBook.sort(sortByState);
+  console.log(addressBook);
+  
+  // Sort the address book by zip
+  addressBook.sort(sortByZip);
+  console.log(addressBook);

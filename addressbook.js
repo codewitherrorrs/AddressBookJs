f371553c27df276bf7eab97cf52b7d20befdf293
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-29 13:41:53 +0530

    Add : UC6 - AddressBook

--- addressbook.js
+++ addressbook.js
@@ -1,27 +1,14 @@
-let contacts = [
-    { name: "John Smith", email: "john@example.com", phone: "123-456-7890" },
-    { name: "Jane Doe", email: "jane@example.com", phone: "987-654-3210" },
-    { name: "Bob Johnson", email: "bob@example.com", phone: "555-555-5555" }
+const addressBook = [
+    { name: 'Alice', phone: '123-456-7890' },
+    { name: 'Bob', phone: '555-555-5555' },
+    { name: 'Charlie', phone: '987-654-3210' },
+    { name: 'David', phone: '555-123-4567' },
+    { name: 'Eve', phone: '555-123-4567' }
   ];
   
-  // Function to delete a contact by name
-  function deleteContact(name) {
-    let index = -1;
-    for (let i = 0; i < contacts.length; i++) {
-      if (contacts[i].name === name) {
-        index = i;
-        break;
-      }
-    }
-    if (index !== -1) {
-      contacts.splice(index, 1);
-      console.log(`Deleted contact: ${name}`);
-    } else {
-      console.log(`Contact not found: ${name}`);
-    }
-  }
+  const numOfContacts = addressBook.reduce((count, contact) => {
+    return count + 1;
+  }, 0);
   
-  // Example usage
-  deleteContact("Jane Doe");
-  console.log(contacts);
+  console.log(numOfContacts); // Output: 5
   
